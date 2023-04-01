073b22a4b9ad4d27f68433489b317591a9099181
Cristian <sonlatin2@gmail.com>
2023-04-01 10:40:16 -0500

    java example ob

---
+++ Suma.java
@@ -0,0 +1,9 @@
+public class Suma {
+    public static int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+    public static void main(String[] args) {
+        int resultado = sumar(2, 4, 6);
+        System.out.println("El resultado de la suma es: " + resultado);
+    }
+}
